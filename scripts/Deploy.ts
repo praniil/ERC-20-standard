3e354a2241c1328c3fa928ffcb7578a389eb4637
praniil <pparajuli15@gmail.com>
2024-07-09 18:44:08 +0545

    feat: added a deployment script

---
+++ scripts/Deploy.ts
@@ -0,0 +1,21 @@
+import hre from "hardhat"
+
+const _name = 'PrCoin';
+const _symbol = 'PRC';
+const _decimals = 2;
+const _totalSupply = 5000;
+
+
+async function deploy() {
+    const Erc20 = await hre.ethers.getContractFactory("ERC20");
+    const erc = await Erc20.deploy(_name, _symbol, _decimals, _totalSupply);
+    const tx = erc.deploymentTransaction();
+    const receipt = await tx?.wait();
+    console.log(receipt);
+    const address = erc.getAddress();
+    return address
+}
+
+deploy().then((a) => {
+    console.log(a);
+})
